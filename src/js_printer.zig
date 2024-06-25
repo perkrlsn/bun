76e7420177161cb43f70ae4cc8b6103d6d4f5a5e
paperdave <paperdave@users.noreply.github.com>
2024-06-25 07:49:12 +0000

    Apply formatting changes

--- src/js_printer.zig
+++ src/js_printer.zig
@@ -3246,17 +3246,17 @@ fn NewPrinter(
                         last.visitRightAndFinish(p);
                     }
                 },
-                .e_inlined_enum =>  |e| {
+                .e_inlined_enum => |e| {
                     p.printExpr(e.value, level, flags);
-                    if(!p.options.minify_whitespace and !p.options.minify_identifiers) {
+                    if (!p.options.minify_whitespace and !p.options.minify_identifiers) {
                         p.print(" /* ");
                         p.print(e.comment);
                         p.print(" */");
                     }
                 },
                 else => {
-                    if(Environment.isDebug)
-                    Output.panic("Unexpected expression of type .{s}", .{@tagName(expr.data)});
+                    if (Environment.isDebug)
+                        Output.panic("Unexpected expression of type .{s}", .{@tagName(expr.data)});
                 },
             }
         }
